1675747359801ce5e58563d12f6d3c69014dcb10
João Miguel <macbookprom@Macbooks-MacBook-Pro.local>
2023-06-11 15:42:52 -0300

    first commit

---
+++ src/components/ReconhecimentoFacial.js
@@ -0,0 +1,72 @@
+import { useRef, useEffect } from 'react'
+import * as faceapi from 'face-api.js'
+
+function ReconhecimentoFacial() {
+  const videoRef = useRef()
+  const canvasRef = useRef()
+
+  // LOAD FROM USEEFFECT
+  useEffect(() => {
+    startVideo()
+    videoRef && loadModels()
+
+  }, [])
+
+
+
+  // OPEN YOU FACE WEBCAM
+  const startVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((currentStream) => {
+        videoRef.current.srcObject = currentStream
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  // LOAD MODELS FROM FACE API
+
+  const loadModels = () => {
+    Promise.all([
+      // THIS FOR FACE DETECT AND LOAD FROM YOU PUBLIC/MODELS DIRECTORY
+      faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+      faceapi.nets.faceRecognitionNet.loadFromUri("/models")
+
+    ]).then(() => {
+      faceMyDetect()
+    })
+  }
+
+  const faceMyDetect = () => {
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(videoRef.current,
+        new faceapi.TinyFaceDetectorOptions())
+
+      // DRAW YOU FACE IN WEBCAM
+      canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(videoRef.current)
+      faceapi.matchDimensions(canvasRef.current, {
+        width: 300,
+        height: 250
+      })
+
+      const resized = faceapi.resizeResults(detections, {
+        width: 300,
+        height: 250
+      })
+
+      faceapi.draw.drawDetections(canvasRef.current, resized)
+    }, 1000)
+  }
+
+  return (
+    <div className='flex flex-col w-full items-center justify-between'>
+      <div className="flex items-center">
+        <video crossOrigin="anonymous" ref={videoRef} autoPlay></video>
+      </div>
+      <canvas ref={canvasRef} width="400" height="400"  className="absolute" />
+    </div>
+  )
+
+}
+
+export default ReconhecimentoFacial;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,36 @@
-import Image from 'next/image'
+import ReconhecimentoFacial from '@/components/ReconhecimentoFacial'
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
+
+import LogoIFS from '../../public/logoIFS.png'
+import Image1 from '../../public/imageLogin1.png'
+import Image2 from '../../public/imageLogin2.png'
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      className={`flex h-screen flex-col items-center justify-center overflow-hidden bg-[#202020] p-24 ${inter.className} `}
+      >
+      <Image src={Image1} alt="" className='fixed h-screen w-auto left-0 mix-blend-lighten blur-[3px]' />
+      <Image src={Image2} alt="" className='fixed h-screen w-auto right-0 mix-blend-lighten blur-[3px]' />
+
+      <div className='flex max-w-lg flex-col items-center shadow-lg bg-black justify-between rounded-xl overflow-hidden px-20 py-14 relative text-center'>
+        <Image src={LogoIFS} alt="Logo of IFS" />
+        <div className='my-6'>
+          <p className='text-xl font-bold uppercase'>Cette page est protégée</p>
+          <p className='text-xs uppercase'>Connectez-vous par reconnaissance faciale</p>
+        </div>
+        <div className='rounded-lg overflow-hidden mb-6'>
+          <ReconhecimentoFacial />
+        </div>
+        <div className='flex flex-col justify-between'>
+          <button className='w-full p-2 px-8 opacity-90 rounded bg-gradient-to-b from-[#B48D41] to-[#755A27] hover:opacity-100'>Scanne mon visage</button>
+          <p className='text-sm mt-4'>Entrer le mot de passe</p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
